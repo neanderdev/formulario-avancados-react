17e9a2ab86c4eaa4f4bc39287593473cd076d722
Neander de Souza <neander.faria@hotmail.com>
2023-04-12 16:50:38 -0300

    Field arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -7,7 +7,7 @@ import './styles/global.css';
 
 /** TO-DO
  * [ X ] Validação / transformação
- * [ ] Field arrays
+ * [ X ] Field arrays
  * [ ] Upload de arquivos
  * [ ] Coomposition Pattern
  */
@@ -24,26 +24,47 @@ const createUserFormSchema = z.object({
     .nonempty('O e-mail é obrigatório')
     .email('Formato de e-mail inválido')
     .refine(email => {
-      return email.endsWith("@gmail.com");
+      return email.endsWith('@gmail.com');
     }, 'O e-mail precisa ser do gmail'),
   password: z.string()
     .min(6, 'A senha precisa do mínimo 6 caracteres'),
+  techs: z.array(z.object({
+    title: z.string().nonempty('O título é obrigatório'),
+    knowledge: z.coerce
+      .number()
+      .min(1, 'No mínimo 1')
+      .max(100, 'No máximo 100 caracteres'),
+  })).min(2, 'Insira pelo menos 2 tecnologias'),
 });
 
 type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export default function App() {
   const [output, setOutput] = useState('');
+
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
+    control,
   } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'techs',
+  });
+
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2));
+  }
+
+  function addNewTech() {
+    append({
+      title: '',
+      knowledge: 0,
+    });
   }
 
   return (
@@ -64,7 +85,7 @@ export default function App() {
           />
 
           {
-            errors.name && <span>{errors.name.message}</span>
+            errors.name && <span className='text-red-500 text-sm'>{errors.name.message}</span>
           }
         </div>
 
@@ -80,7 +101,7 @@ export default function App() {
           />
 
           {
-            errors.email && <span>{errors.email.message}</span>
+            errors.email && <span className='text-red-500 text-sm'>{errors.email.message}</span>
           }
         </div>
 
@@ -96,7 +117,60 @@ export default function App() {
           />
 
           {
-            errors.password && <span>{errors.password.message}</span>
+            errors.password && <span className='text-red-500 text-sm'>{errors.password.message}</span>
+          }
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='tecnologias' className='flex justify-between items-center'>
+            Tecnologias
+
+            <button
+              type='button'
+              className='text-emerald-500 text-sm'
+              onClick={addNewTech}
+            >
+              Adicionar
+            </button>
+          </label>
+
+          {
+            fields.map((field, index) => {
+              return (
+                <div
+                  key={String(field.id)}
+                  className='flex gap-2'
+                >
+                  <div className='flex-1 flex flex-col gap-1'>
+                    <input
+                      className='border border-zinc-200 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white'
+                      type='text'
+                      {...register(`techs.${index}.title`)}
+                    />
+
+                    {
+                      errors.techs?.[index]?.title && <span className='text-red-500 text-sm'>{errors.techs?.[index]?.title?.message}</span>
+                    }
+                  </div>
+
+                  <div className='w-20 flex flex-col gap-1'>
+                    <input
+                      className='border border-zinc-200 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white'
+                      type='number'
+                      {...register(`techs.${index}.knowledge`)}
+                    />
+
+                    {
+                      errors.techs?.[index]?.knowledge && <span className='text-red-500 text-sm'>{errors.techs?.[index]?.knowledge?.message}</span>
+                    }
+                  </div>
+                </div>
+              )
+            })
+          }
+
+          {
+            errors.techs && <span className='text-red-500 text-sm'>{errors.techs.message}</span>
           }
         </div>
 
